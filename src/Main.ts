126ec4126a2343a4e12cd3fe704d3ed5dbd0dc96
Franco Barletta <franco.barletta@dubitlimited.com>
2017-03-02 23:32:33 -0300

    Add intro text.

--- src/Main.ts
+++ src/Main.ts
@@ -1,43 +1,53 @@
 import { Application } from './render/Application';
 import { Game } from './shooter/Game';
-import { Container, Text, Rectangle, autoDetectRenderer } from "pixi.js";
+import { Container, Text, Rectangle, autoDetectRenderer, Graphics } from "pixi.js";
 
-class Main extends Container {
+class Main {
 
 	private introText: Text;
-	private stage: Container;
 	private app: Application;
 
 	constructor() {
-		super();
 		console.log("Main");
 		//create Render
 		this.app = new Application(800, 600);
-		//this.init();
-		this.startGame();
+		this.init();
+
 	}
 
 
 	private init(): void {
-	
-		this.introText = new Text();
-		this.introText.text = "Click to start\nGame controls:WASD, mouse move and left click";
 
-		var textBounds: Rectangle = this.introText.getBounds();
+		//interactive zone
+		var stageHitArea: Graphics = new Graphics();
+		stageHitArea.beginFill(0xFFFFFF, 0);
+		stageHitArea.drawRect(0, 0, this.app.width, this.app.height);
+		stageHitArea.interactive = true;
+		this.app.stage.addChild(stageHitArea);
+		
+		//add intro text
+		this.introText = new Text("Game controls:\n\tW, A, S and D,\n\tmouse move and left click\n\ntscClick to start",
+			{
+				font: "20",
+				fill: 0xFFFFFF,
+				align: "center"
+			});
+
 		this.introText.anchor.y = this.introText.anchor.x = 0.5;
-		this.introText.x = 800 / 2;
-		this.introText.y = 600 / 2;
+		this.introText.x = this.app.width * 0.5;
+		this.introText.y = this.app.height * 0.5;
 
 		this.showIntro();
 	}
 
 	private showIntro(): void {
-		this.addChild(this.introText);
+		this.app.stage.addChild(this.introText);
+		this.app.stageIteraction.onClick.addOnce(this.startGame, this);
 	}
 
 	private startGame(): void {
-		this.removeChild(this.introText);
-		var game: Game = new Game(this.app.stage, this.app.width, this.app.height, this.app.onUpdate, this.app);
+		this.app.stage.removeChild(this.introText);
+		var game: Game = new Game(this.app.stage, this.app.width, this.app.height, this.app.onUpdate, this.app.stageIteraction);
 	}
 
 }
